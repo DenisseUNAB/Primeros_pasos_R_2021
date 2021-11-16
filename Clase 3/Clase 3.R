d332c9e6bf6af42615898b0cb883c58ac1f01f29
Diego Muñoz <51221163+DiegoHoliwis@users.noreply.github.com>
2021-11-16 19:03:13 -0300

    Add files via upload

---
+++ Clase 3/Clase 3.R
@@ -0,0 +1,6 @@
+# Profesor Diego Muñoz - dimunoz1@uc.cl
+# Ayudante Felipe Moya - felipe.moya@uc.cl
+
+# Clase 3
+# Repositorio GitHub: https://github.com/DiegoHoliwis/Primeros_pasos_R_2021
+# Presentación: https://diegoholiwis.github.io/Primeros_pasos_R_2021/Clase_3.html
