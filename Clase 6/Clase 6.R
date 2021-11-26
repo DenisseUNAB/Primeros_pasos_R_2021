f976c9865dd462ec7f760f0187277787c0e8bcc3
Diego Muñoz <dimunozu@gmail.com>
2021-11-25 21:02:31 -0300

    .

--- Clase 6/Clase 6.R
+++ Clase 6/Clase 6.R
@@ -11,7 +11,7 @@
 library(ggplot2)
 library(dplyr)
 library(datos)
-
+`%notin%` <- Negate(`%in%`)
 
 paises %>% 
   ggplot(aes(x = esperanza_de_vida)) +
@@ -51,9 +51,50 @@ paises %>%
   ggtitle("Mi segundo gr??fico")
 
 
+paises %>% 
+  filter(pais == "Chile") %>% 
+  ggplot(aes(x = anio, y = esperanza_de_vida)) + 
+  geom_line() +
+  geom_point(size = 6, alpha = .4, col = "#0F55BB") +
+  labs(title = "Esperanza de vida en Chile", 
+       subtitle = "A??os 1952 a 2007",
+       x = "A??o",
+       y = "Esperanza de vida") 
+
+# plotly::ggplotly()
+
+paises %>% 
+  ggplot(aes(x = esperanza_de_vida, y = pib_per_capita)) +
+  geom_point(aes(color = continente, size = poblacion),
+             alpha = .4)
 
 
 
+paises %>% 
+  filter(pais %in% c("Chile","Per??","Argentina","Bolivia")) %>% 
+  ggplot(aes(x = esperanza_de_vida, y = pib_per_capita)) +
+  geom_point(aes(color = poblacion))
 
 
+paises %>% 
+  filter(pais %in% c("Chile","Per??","Argentina","Bolivia")) %>% 
+  ggplot(aes(x = anio, y = esperanza_de_vida, color = pais) ) +
+  geom_point(size = 3) +
+  geom_line(size = 1) +
+  theme_bw() + 
+  theme(legend.position   = "top",
+        legend.title      = element_text(colour = "red", size = 30),
+        legend.background = element_rect(fill="#0F55BB"),
+        axis.text = element_text(size = 15)) 
+
+
+# paises %>% 
+#   filter(pais == "Chile" |
+#            pais == "Per??"|
+#            pais == "Argentina"|
+#            pais == "Bolivia")
+# 
+# 
+# paises %>% 
+#   filter(pais %in% c("Chile","Per??","Argentina","Bolivia"))
 
